3067c7a41384f395d5f1904a0fb90d04babfb6ba
xAffinity <yakkiansheng@gmail.com>
2018-03-14 23:16:49 +0000

    Set up dependencies for development

---
+++ Project1-Splitter/truffle.js
@@ -0,0 +1,25 @@
+var DefaultBuilder = require("truffle-default-builder");
+
+// Allows us to use ES6 in our migrations and tests.
+require('babel-register')
+
+module.exports = {
+  build: new DefaultBuilder({
+  	"index.html": "index.html",
+  	"app.js": [
+  		"javascripts/_vendor/angular.min.js",
+  		"javascripts/app.js"
+  	],
+  	"app.css": [
+  		"stylesheets/app.css"
+  	],
+  	"images/": "images/"
+  }),
+  networks: {
+    development: {
+      host: '127.0.0.1',
+      port: 7545,
+      network_id: '*' // Match any network id
+    }
+  }
+}
